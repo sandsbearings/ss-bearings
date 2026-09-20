cecb30b115bcb1937cd7779e497d5bd353865ca8
sandsbearings <sandsbearings@gmail.com>
2026-09-21 02:50:58 +0530

    Force IPv4 for Gmail SMTP to fix ENETUNREACH on Render

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -3,6 +3,9 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_APP_PASSWORD },
+  // Some hosts (e.g. Render) can't route to Gmail's IPv6 SMTP address and fail with
+  // ENETUNREACH; forcing IPv4 avoids that.
+  family: 4,
 });
 
 export async function sendEmail({ to, subject, html }) {
